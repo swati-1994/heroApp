e838505740625702f539bdf014cf60f65f156920
swati sharma <swati@headerlabs.com>
2016-11-20 14:49:01 +0530

    promise error in services

---
+++ app/mock-novels.ts
@@ -0,0 +1,10 @@
+import { Novel } from './novel';
+
+export const NOVELS:Novel[] = [
+    {type: 'Scientific', name: 'Deception Point'},
+    {type: 'Thriller', name: 'Girl with the dragon tattoo'},
+    {type: 'thriller', name: 'if tomorrow comes'},
+    {type: 'fiction', name: 'Inferno'},
+    {type: 'fiction', name: 'Twilight'},
+    {type: 'fiction', name: 'Thousand Splendid suns'}
+];
